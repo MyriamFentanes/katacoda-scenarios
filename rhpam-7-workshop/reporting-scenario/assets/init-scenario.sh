6b7fff6e8acb61693603aa8977369c0f42e5cbc1
Duncan Doyle <Duncan.Doyle@gmail.com>
2018-09-11 16:41:47 +0200

    Refactored init-scenario for PAM 7.

--- rhpam-7-workshop/reporting-scenario/assets/init-scenario.sh
+++ rhpam-7-workshop/reporting-scenario/assets/init-scenario.sh
@@ -6,14 +6,14 @@ echo
 echo "Waiting for the Red Hat Process Automation Manager workbench OpenShift Container to start"
 until oc project rhpam7-workshop &>2; do echo -n . && sleep 5; done
 until oc get dc/rhpam7-workshop-rhdmcentr &>2; do echo -n . && sleep 5; done
-oc rollout status "dc/rhpam7-workshop-rhdmcentr" -n loan-demo
+oc rollout status "dc/rhpam7-workshop-rhpamcentr" -n rhpam7-workshop
 echo
 
 #Wait for Decision Manager workbench availability
-echo "Waiting for the Red Hat Decision Manager workbench to become available"
-export DC_HOST=$(oc describe route "loan-demo-rhdmcentr" | grep "Requested Host" | sed 's/Requested Host://' | tr -d '[:blank:]')
+echo "Waiting for the Red Hat Process Automation Manager workbench to become available"
+export BC_HOST=$(oc describe route "rhpam7-workshop-rhpamcentr" | grep "Requested Host" | sed 's/Requested Host://' | tr -d '[:blank:]')
 #until [ $(curl -sL -w "%{http_code}\\n" "http://$DC_HOST/kie-drools-wb.jsp" -o /dev/null) == 200 ]; do echo -n . && sleep 5; done
-until curl -s http://$DC_HOST/kie-drools-wb.jsp | grep -q "Decision"; do echo -n . && sleep 5; done
+until curl -s http://$BC_HOST/kie-drools-wb.jsp | grep -q "Business"; do echo -n . && sleep 5; done
 echo
 
 echo "Enviroment ready!"
