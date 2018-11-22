4e31e52f78b17d6e80439a9a5f8b07380a3dd9b6
Duncan Doyle <Duncan.Doyle@gmail.com>
2018-11-22 10:06:45 +0100

    Added OpenShift provisioning of pre-populated PostgreSQL environment.

--- rhpam-7-workshop/reporting-scenario/env-init.sh
+++ rhpam-7-workshop/reporting-scenario/env-init.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
+
+
+# Import ImageStreams
 ssh root@host01 'echo "Importing Red Hat Process Automation Manager 7 Image Streams into OpenShift." >> script.log'
-# Need to temporarily patch the ImageStreams file to point to tech-preview version.
-ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-container-images/rhpam-7-openshift-image/7.0.2.GA/rhpam70-image-streams.yaml -n openshift && break || sleep 2; done'
+ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-container-images/rhpam-7-openshift-image/7.1.0.GA/rhpam70-image-streams.yaml -n openshift && break || sleep 2; done'
+
+# Import Templates
 ssh root@host01 'echo "Importing Red Hat Process Automation Manager 7 - Trial template into OpenShift." >> script.log'
-#Remove persistent volume claim from the template, as we do not have PVC in Katacoda
-#ssh root@host01 'for i in {1..200}; do wget https://raw.githubusercontent.com/jboss-container-images/rhdm-7-openshift-image/7.0.1.GA/templates/rhdm70-full.yaml && break || sleep 2; done'
-#ssh root@host01 'for i in {1..200}; do cat rhdm70-full.yaml | sed "/rhdmcentr-pvol/,+1 d" | sed "/claimName/d" | sed "s/haproxy.router.openshift.io\/timeout: 60s/haproxy.router.openshift.io\/timeout: 600s/g" | head -n -12 > rhdm70-full-no-persistence.yaml && break || sleep 2; done'
-#ssh root@host01 'for i in {1..200}; do oc create -f rhdm70-full-no-persistence.yaml -n openshift && break || sleep 2; done'
 ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-container-images/rhpam-7-openshift-image/7.0.2.GA/templates/rhpam70-trial-ephemeral.yaml -n openshift && break || sleep 2; done'
+ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/openshift/origin/v3.7.0/examples/db-templates/postgresql-ephemeral-template.json -n openshift && break || sleep 2; done'
+
 ssh root@host01 'echo "Logging into OpenShift as developer." >> script.log'
 ssh root@host01 'for i in {1..200}; do oc login -u developer -p developer && break || sleep 2; done'
+
+# Create new project
 ssh root@host01 'echo "Creating new demo project in OpenShift." >> script.log'
 ssh root@host01 'for i in {1..200}; do oc new-project rhpam7-workshop --display-name="RHPAM 7 Reporting" --description="Red Hat Process Automation Manager 7 - Reporting Project" && break || sleep 2; done'
 ssh root@host01 'echo "Importing secrets and service accounts."'
-#ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-container-images/rhdm-7-openshift-image/7.0.1.GA/decisioncentral-app-secret.yaml && break || sleep 2; done'
-#ssh root@host01 'for i in {1..200}; do oc create -f https://raw.githubusercontent.com/jboss-container-images/rhdm-7-openshift-image/7.0.1.GA/kieserver-app-secret.yaml && break || sleep 2; done'
+
+# Create PAM containers.
 ssh root@host01 'echo "Creating Business Central and KIE Server containers in OpenShift." >> script.log'
-ssh root@host01 'for i in {1..200}; do oc new-app --template=rhpam70-trial-ephemeral -p APPLICATION_NAME="rhpam7-workshop" -p IMAGE_STREAM_NAMESPACE="openshift" -p KIE_ADMIN_USER="developer" -p KIE_SERVER_CONTROLLER_USER="kieserver" -p KIE_SERVER_USER="kieserver" -p DEFAULT_PASSWORD="developer" -p MAVEN_REPO_USERNAME="developer" -p MAVEN_REPO_PASSWORD="developer" -p BUSINESS_CENTRAL_MEMORY_LIMIT="2Gi" -e JAVA_OPTS_APPEND=-Derrai.bus.enable_sse_support=false -n rhpam7-workshop && break || sleep 2; done'
+ssh root@host01 'for i in {1..200}; do oc new-app --template=rhpam71-trial-ephemeral -p APPLICATION_NAME="rhpam7-workshop" -p IMAGE_STREAM_NAMESPACE="openshift" -p KIE_ADMIN_USER="developer" -p KIE_SERVER_CONTROLLER_USER="kieserver" -p KIE_SERVER_USER="kieserver" -p DEFAULT_PASSWORD="developer" -p MAVEN_REPO_USERNAME="developer" -p MAVEN_REPO_PASSWORD="developer" -p BUSINESS_CENTRAL_MEMORY_LIMIT="2Gi" -e JAVA_OPTS_APPEND=-Derrai.bus.enable_sse_support=false -n rhpam7-workshop && break || sleep 2; done'
 ssh root@host01 'echo "Patching Business Central OpenShift route to increase proxy timeout." >> script.log'
 ssh root@host01 'for i in {1..200}; do oc annotate route rhpam7-workshop-rhpamcentr --overwrite haproxy.router.openshift.io/timeout=600s -n rhpam7-workshop && break || sleep 2; done'
+
+# Create PostgreSQL container
+ssh root@host01 'for i in {1..200}; do oc new-app --template=postgresql-ephemeral -p NAMESPACE=openshift -p POSTGRESQL_DATABASE="rhpam7_workshop_reporting" -p POSTGRESQL_USER=postgres -p POSTGRESQL_PASSWORD=postgres -n rhpam7-workshop && break || sleep 2; done'
+ssh root@host01 'for i in {1..200}; do oc create configmap postgresql-config-map --from-file=~/.init/provision_data.sh --from-file=~/.init/wait_for_postgres.sh --from-file=~/.init/provision_test_data.sql -n rhpam7-workshop && break || sleep 2; done'
+ssh root@host01 'for i in {1..200}; do oc volume dc/postgresql --name=postgresql-config-volume --add -m /tmp/config-files -t configmap --configmap-name=postgresql-config-map && break || sleep 2; done'
+ssh root@host01 'for i in {1..200}; do oc set deployment-hook dc/postgresql --post -c postgresql -e POSTGRESQL_HOSTNAME=postgresql -e POSTGRESQL_USER=postgres -e POSTGRESQL_PASSWORD=postgres --volumes=postgresql-config-volume --failure-policy=abort -- /bin/bash /tmp/config-files/wait_for_postgres.sh /tmp/config-files/provision_data.sh && break || sleep 2; done'
