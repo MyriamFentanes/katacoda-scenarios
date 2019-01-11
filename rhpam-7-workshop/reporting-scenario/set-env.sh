525bc69de2cfc5ede82ae66416b61752ff20a83f
Duncan Doyle <Duncan.Doyle@gmail.com>
2019-01-11 09:27:14 +0100

    Fixed http routing on Katacoda.

--- rhpam-7-workshop/reporting-scenario/set-env.sh
+++ rhpam-7-workshop/reporting-scenario/set-env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 until [ -f /openshift.local.config/master/master-config.yaml ] ; do echo -n . && sleep 5; done
-sed -i '230s/\.environments\.katacoda\.com/.http-proxy.katacoda.com/' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
+sed -i '213s/\.environments\.katacoda\.com/.http-proxy.katacoda.com/' /openshift.local.config/master/master-config.yaml && systemctl restart origin && ~/.launch.sh
 ~/.init/init-scenario.sh
