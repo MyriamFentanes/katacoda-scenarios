865b5caef7df6ce04b4b0dedea83ee0419eba367
MyriamFentanes <myriam.fentanes@gmail.coms>
2019-02-11 16:54:14 -0500

    Adding content to the installation

--- rhpam-7-workshop/case-management-scenario/assets/init-scenario.sh
+++ rhpam-7-workshop/case-management-scenario/assets/init-scenario.sh
@@ -4,14 +4,14 @@ echo
 
 #Wait for Decision Central OpenShift deployment
 echo "Waiting for the Red Hat Process Automation Manager workbench OpenShift Container to start"
-until oc project rhpam7-workshop &>2; do echo -n . && sleep 5; done
-until oc get dc/rhpam7-workshop-rhpamcentr &>2; do echo -n . && sleep 5; done
-oc rollout status "dc/rhpam7-workshop-rhpamcentr" -n rhpam7-workshop
+until oc project ccd-project &>2; do echo -n . && sleep 5; done
+until oc get dc/ccd-project-rhpamcentr &>2; do echo -n . && sleep 5; done
+oc rollout status "dc/ccd-project-rhpamcentr" -n ccd-project
 echo
 
 #Wait for Decision Manager workbench availability
 echo "Waiting for the Red Hat Process Automation Manager workbench to become available"
-export BC_HOST=$(oc describe route "rhpam7-workshop-rhpamcentr" | grep "Requested Host" | sed 's/Requested Host://' | tr -d '[:blank:]')
+export BC_HOST=$(oc describe route "ccd-project-rhpamcentr" | grep "Requested Host" | sed 's/Requested Host://' | tr -d '[:blank:]')
 #until [ $(curl -sL -w "%{http_code}\\n" "http://$DC_HOST/kie-drools-wb.jsp" -o /dev/null) == 200 ]; do echo -n . && sleep 5; done
 until curl -s http://$BC_HOST/kie-wb.jsp | grep -q "Business"; do echo -n . && sleep 5; done
 echo
