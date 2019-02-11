3e96db418faf3fb394f9aed3a79a441dcf51fbf8
MyriamFentanes <myriam.fentanes@gmail.coms>
2019-02-11 10:41:30 -0500

    Adding content to the installation

--- rhpam-7-workshop/case-management-scenario/courseBase.sh
+++ rhpam-7-workshop/case-management-scenario/courseBase.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ssh root@host01 'echo "Pulling the local environment for Red Hat Process Automation Manager" >> script.log'
-#ssh root@host01 'docker pull quay.io/mfentanes/rhpam7-katacoda'
+ssh root@host01 'docker pull quay.io/mfentanes/rhpam7-katacoda'
 ssh root@host01 'echo "Running the local environment for Red Hat Process Automation Manager" >> script.log'
-#ssh root@host01 'docker run -p 8080:8080 quay.io/mfentanes/rhpam7-katacoda'
+ssh root@host01 'docker run -p 8080:8080 quay.io/mfentanes/rhpam7-katacoda'
